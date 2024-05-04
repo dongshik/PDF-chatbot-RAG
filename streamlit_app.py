8315bfbb8ccfa8a0fac16c953a5ba1d73a236395
Gabriel Lee <dongshik.lee@gmail.com>
2024-05-04 19:34:52 +0900

    M1 my notebook
    First run

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,18 +9,26 @@ from langchain.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 
+import os
+from dotenv import load_dotenv
+load_dotenv()
+#openai_api_key = os.environ['OPENAI_API_KEY']
 
 #function to load the vectordatabase
 def load_knowledgeBase():
-        embeddings=OpenAIEmbeddings(api_key="Enter your api key")
+        #embeddings=OpenAIEmbeddings(api_key="Enter your api key")
+        #embeddings=OpenAIEmbeddings(api_key=openai_api_key)
+        embeddings=OpenAIEmbeddings()
         DB_FAISS_PATH = 'vectorstore/db_faiss'
-        db = FAISS.load_local(DB_FAISS_PATH, embeddings)
+        db = FAISS.load_local(DB_FAISS_PATH, embeddings, allow_dangerous_deserialization=True)
         return db
         
 #function to load the OPENAI LLM
 def load_llm():
         from langchain_openai import ChatOpenAI
-        llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, api_key="Enter your api key")
+        #llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, api_key="Enter your api key")
+        #llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, api_key=openai_api_key)
+        llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
         return llm
 
 #creating prompt template using langchain
@@ -48,12 +56,11 @@ if __name__=='__main__':
         
         query=sl.text_input('Enter some text')
         
-        
         if(query):
                 #getting only the chunks that are similar to the query for llm to produce the output
                 similar_embeddings=knowledgeBase.similarity_search(query)
-                similar_embeddings=FAISS.from_documents(documents=similar_embeddings, embedding=OpenAIEmbeddings(api_key="Enter your api key"))
-                
+                #similar_embeddings=FAISS.from_documents(documents=similar_embeddings, embedding=OpenAIEmbeddings(api_key="Enter your api key"))
+                similar_embeddings=FAISS.from_documents(documents=similar_embeddings, embedding=OpenAIEmbeddings())
                 #creating the chain for integrating llm,prompt,stroutputparser
                 retriever = similar_embeddings.as_retriever()
                 rag_chain = (
--- vector_loader.py
+++ vector_loader.py
@@ -5,13 +5,21 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings
 
+import os
+from dotenv import load_dotenv
+load_dotenv()
+#print("OPENAI API Key : {}".format(os.environ['OPENAI_API_KEY']))
+#openai_api_key = os.environ['OPENAI_API_KEY']
 
 #create a new file named vectorstore in your current directory.
 if __name__=="__main__":
+
         DB_FAISS_PATH = 'vectorstore/db_faiss'
         loader=PyPDFLoader("./random machine learing pdf.pdf")
         docs=loader.load()
         text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
         splits = text_splitter.split_documents(docs)
-        vectorstore = FAISS.from_documents(documents=splits, embedding=OpenAIEmbeddings(api_key="Enter your api key"))
+        #vectorstore = FAISS.from_documents(documents=splits, embedding=OpenAIEmbeddings(api_key="Enter your api key"))
+        #vectorstore = FAISS.from_documents(documents=splits, embedding=OpenAIEmbeddings(api_key=openai_api_key))
+        vectorstore = FAISS.from_documents(documents=splits, embedding=OpenAIEmbeddings())
         vectorstore.save_local(DB_FAISS_PATH)
